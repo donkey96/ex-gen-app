f71868d38bb74f9c14903470d453f04fc06cfb19
donkey96 <[noire964@gmail.com]>
2020-05-26 13:40:44 +0900

    /addの処理

--- routes/hello.js
+++ routes/hello.js
@@ -11,6 +11,7 @@ var mysql_setting = {
   database : 'my-nodeapp-db'
 };
 
+// デフォルトページ
 router.get('/', (req, res, next) => {
 
   // コネクションの用意
@@ -28,6 +29,37 @@ router.get('/', (req, res, next) => {
         res.render('hello/index', data);
       }
   });
+  // 接続を解除
+  connection.end();
+});
+
+// 新規作成ページへのアクセス
+router.get('/add', (req, res, next) => {
+  var data = {
+    title: 'Hello/Add',
+    content: '新しいレコードを入力:'
+  }
+  res.render('hello/add', data);
+});
+
+// 新規作成フォーム送信の処理
+router.post('/add', (req, res, next) => {
+  var nm = req.body.name;
+  var ml = req.body.mail;
+  var ag = req.body.age;
+  var data = { 'name': nm, 'mail': ml, 'age': ag };
+
+  // データベースの設定情報
+  var connection = mysql.createConnection(mysql_setting);
+
+  // データベースに接続
+  connection.connect();
+
+  // データを取り出す
+  connection.query('insert into mydata set ?', data,
+    function (error, results, fields) {
+      res.redirect('/hello');
+    });
   // 接続を解除
   connection.end();
 });
