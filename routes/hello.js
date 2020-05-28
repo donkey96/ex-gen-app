8bd01e0b4e2df3290cc76a1082b07935a530b48a
donkey96 <[noire964@gmail.com]>
2020-05-28 11:52:23 +0900

    /helloをページ分けして表示する

--- routes/hello.js
+++ routes/hello.js
@@ -205,4 +205,24 @@ router.post('/find', (req, res, next) => {
   })
 });
 
+Bookshelf.plugin('pagination'); // fetchPage追加
+
+router.get('/:page', (req, res, next) => {
+  var pg = req.params.page;
+  pg *= 1;
+  if (pg < 1) { pg = 1 }
+  new MyData().fetchPage({ page: pg, pageSize: 3 }).then((collection) => {
+    var data = {
+      title: 'Hello!',
+      content: collection.toArray(),
+      pagination: collection.pagination
+    };
+    console.log(collection.pagination);
+    res.render('hello/index', data);
+  })
+  .catch((err) => {
+    res.status(500).json({ error: true, data: { message: err.message } });
+  });
+});
+
 module.exports = router;
